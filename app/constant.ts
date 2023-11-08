55d5a974d9a29939545029ec6207e149bd445a25
yszddai <372767021@qq.com>
2023-11-08 11:12:48 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -8,6 +8,7 @@ export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/c
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 export const DEFAULT_API_HOST = "https://chatgpt1.nextweb.fun/api/proxy";
+export const DEFAULT_CORS_HOST = "https://ab.nextweb.fun";
 
 export enum Path {
   Home = "/",
